3b1fd4a84df6391b6db45807670a04c71efff791
graceku72 <gku@unc.edu>
2023-09-10 17:25:40 +0000

    worked on registration and confirmation pages

--- frontend/src/app/confirmation/confirmation.component.ts
+++ frontend/src/app/confirmation/confirmation.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css']
 })
-export class ConfirmationComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit(): void {
-    //const routeParams = this.route.snapshot.paramMap: const 
+export class ConfirmationComponent {
+  constructor(private router: Router) {
+    
   }
+
+  home() {
+    this.router.navigate(['/']);
+  }
+  // ngOnInit(): void {
+  //   //const routeParams = this.route.snapshot.paramMap: const 
+  // }
+
+  // constructor(private router: Router) {
+    
+  // }
 }
