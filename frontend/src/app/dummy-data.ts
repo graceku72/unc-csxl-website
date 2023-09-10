a33e94c48320992bc73e19dad2e28bd63d036c17
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2023-09-10 22:38:18 +0000

    Stats page done

---
+++ frontend/src/app/dummy-data.ts
@@ -0,0 +1,132 @@
+// dummy-data.ts
+import { User } from './user'; // Import the User interface if it's in a separate file
+
+export const dummyUsers: User[] = [
+  {
+    name: 'John Doe',
+    pid: 730472629,
+    email: 'john@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Jane Smith',
+    pid: 730472629,
+    email: 'jane@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Alice Johnson',
+    pid: 730472629,
+    email: 'alice@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Bob Brown',
+    pid: 730472629,
+    email: 'bob@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Eve Davis',
+    pid: 730472629,
+    email: 'eve@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Charlie Wilson',
+    pid: 730472629,
+    email: 'charlie@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Grace Lee',
+    pid: 730472629,
+    email: 'grace@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'David Smith',
+    pid: 730472629,
+    email: 'david@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Fiona Brown',
+    pid: 730472629,
+    email: 'fiona@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'George Miller',
+    pid: 730472629,
+    email: 'george@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Hannah White',
+    pid: 730472629,
+    email: 'hannah@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Isaac Johnson',
+    pid: 730472629,
+    email: 'isaac@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Julia Davis',
+    pid: 730472629,
+    email: 'julia@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Kevin Wilson',
+    pid: 730472629,
+    email: 'kevin@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Lily Smith',
+    pid: 730472629,
+    email: 'lily@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Michael Adams',
+    pid: 730472629,
+    email: 'michael@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Natalie Davis',
+    pid: 730472629,
+    email: 'natalie@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Oliver Brown',
+    pid: 730472629,
+    email: 'oliver@example.com',
+    type: 'Admin',
+  },
+  {
+    name: 'Patricia Johnson',
+    pid: 730472629,
+    email: 'patricia@example.com',
+    type: 'Regular',
+  },
+  {
+    name: 'Quincy White',
+    pid: 730472629,
+    email: 'quincy@example.com',
+    type: 'Admin',
+  },
+  // Add 1 more user with PID 730472629
+  {
+    name: 'User 21',
+    pid: 730472629,
+    email: 'user21@example.com',
+    type: 'Regular',
+  },
+];
--- frontend/src/app/stats/stats.component.ts
+++ frontend/src/app/stats/stats.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { dummyUsers } from '../dummy-data';
 
 @Component({
   selector: 'app-stats',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./stats.component.css']
 })
 export class StatsComponent {
-
+  users = dummyUsers;
 }
